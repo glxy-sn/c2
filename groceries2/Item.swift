e8a81db87a47ae7b4df8f361b0309d3610bf347c
Tiara <tiarashafa6@gmail.com>
2026-05-07 13:14:11 +0700

    Initial Commit

---
+++ groceries2/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  groceries2
+//
+//  Created by Shafa Tiara on 07/05/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
